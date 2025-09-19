54dd534e465960ff6969ab9542a3fa0cf485c2e7
Lugia11 <noahgunnarsson2011@hotmail.com>
2025-09-19 11:10:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-output_div = document.querySelector("#output")
+from pyscript import Element
 
-output_div.innerText = "I hate them blackies"
+# Get the div element by its ID
+output_div = Element("output")
+
+# Set the inner text of the div
+output_div.write("Hello world")
